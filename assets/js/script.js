340a33a4271e0ebf814503ed057d9c87fdb26ff2
Rodney-D-Dev <>
2023-06-14 17:15:07 +0000

    code commenting and fixes

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,5 +1,5 @@
 
-//Map
+//Walls Layout in an array of # strings
 let levelOne = [
     '##############################',
     '#                            #',
@@ -32,11 +32,12 @@ let levelOne = [
     '#                            #',
     '##############################',
 ]
-// varibles to keep of grid/map size
+//varibles for Play area tile sizes. width * height resulting in grid size;
 let width = 30;
 let height = 30;
-let tileSize = 15;
-// Drawing canvas on screen and adjusting size by width * height
+let tileSize = 20;
+
+//Drawing canvas on screen and adjusting size by width * height and setting border color.
 const rootstyle = getComputedStyle(document.body);
 const gameBoard = document.getElementById("gameBoard");
 const contx = gameBoard.getContext("2d");
@@ -44,17 +45,18 @@ gameBoard.height = height * tileSize;
 gameBoard.width = width * tileSize;
 gameBoard.style.border = `1px solid ${rootstyle.getPropertyValue('--primary-color')}`;
 
-//Game Menus 
+//Game Menus acessed html div elements by Id 
 const startUI = document.getElementById("startMenu");
 const gameOverUI = document.getElementById("gameOverMenu");
 
+//acessing html score,level and lives display
 const scoreDis = document.getElementById("score");
 const levelDis = document.getElementById("level");
 const livesDis = document.getElementById("lives");
 
-//Sound effects
-let eatEffect = new Audio("assets/sounds/carrotnom-92106.mp3");
-let hitEffect = new Audio("assets/sounds/punch-2-37333.mp3");
+//Sound effects adding audio from sounds folder
+const eatEffect = new Audio("assets/sounds/carrotnom-92106.mp3");
+const hitEffect = new Audio("assets/sounds/punch-2-37333.mp3");
 
 // Stats
 let score = 0;
@@ -64,62 +66,67 @@ let lives = 3;
 let currentlives;
 
 //Snake varibles 
-let snake = randomPos(); // snake start point
+let snake = {x:120, y:200}; // snake start point
 let snakeBody = [];
 let snakeSpeed = 10;
+
 //Food Varibles
 let food = randomPos();;
 
-//walls 
+//walls array and wall object with x and y cords  
 let walls = [];
 let wall = {
     x: tileSize,
     y: tileSize
 };
 
-//directinal control
+//input varible to move snake and store last input
 let inputDr = { x: 0, y: 0 };
 let lastInputDr = { x: 0, y: 0 };
 
-
+//helper varibles to be used with setInterval()
 let fPS = 1000 / snakeSpeed;
-let gameLoop = 0;
+let gameLoop;
 
+//varible to help with game state
 let isPaused = false;
 let isGameOver = false;
 
+update(); // called here to allow input and game to be displayed.
 
-update();
-input();
-
+/**
+ * Game Start funtion Handles Game start and sets of Update to run perframe
+ */
 function gameStart() {
     hideMenu(startUI);
     inputDr.x = 0;
     inputDr.y = 0;
     snakeBody.length = 1;
-    snake = randomPos();
+    snake = {x:120, y:220};
     food = randomPos();
     gameLoop = setInterval(update, fPS);
     if (lives <= 0 ) {
         lives = 3;
-        drawGame();
     }
-
 }
+/**
+ * Update function Handles anything that needs to be updated evry frame.
+ */
 function update() {
     if (!isPaused) {
         moveSnake();
         collision();
         currentlives = lives;
+        drawGame();
     }
-    drawGame();
-
+    input();
 }
-
+/**
+ * Draw fuction Handles anything that needs to be drawn to canvas.
+ */
 function drawGame() {
     contx.fillStyle = "white";
     contx.fillRect(0, 0, gameBoard.width, gameBoard.height);
-    createMap(levelOne);//Drawing Walls on canvas 
     drawStats();
     drawMap();
     drawFood();
@@ -128,16 +135,15 @@ function drawGame() {
 function drawStats() {
     scoreDis.innerHTML = `score:${score}`;
     levelDis.innerHTML = `Level:${level}`;
-    for (i = 0; i <= currentlives; i++) {
+    for (i = 0; i <= lives; i++) {
         let heart = "&#128150";
         livesDis.innerHTML = `${heart.repeat(i)}`;
     }
 
 }
 /**
- * 
- * 
- * @param{Array<string>}
+ * Create Map Function to generate walls out of # strings by looping throgh array of strings 
+ * @param {Array [string]}
  */
 function createMap(map) {
     for (let y = 0; y < map.length; y++) {
@@ -151,6 +157,7 @@ function createMap(map) {
     }
 }
 function drawMap() {
+    createMap(levelOne); //create walls out of levlOne array
     for (let i = 0; i < walls.length; i++) {
         const wall = walls[i];
         contx.fillStyle = "#ffc0cb"
@@ -158,12 +165,10 @@ function drawMap() {
     }
 }
 function drawFood() {
-    //Drawing Food on Canvas before snake for collision detection
     contx.fillStyle = "brown";
     contx.fillRect(food.x, food.y, tileSize, tileSize);
 }
 function drawSnake() {
-    //Drawing Snake on Canvas
     contx.fillStyle = "red";
     contx.fillRect(snake.x, snake.y, tileSize, tileSize);
     for (let i = snakeBody.length - 1; i > 0; i--) {
@@ -178,29 +183,33 @@ function drawSnake() {
     }
 
 }
-// snake movement
+/**
+ * Move Snake function to update snake movement
+ */
 function moveSnake() {
 
     snake.x += inputDr.x * tileSize;
     snake.y += inputDr.y * tileSize;
 
 }
-/**hellper function to generate random position on map
- * used for snake and food pos
+/**
+ * RandomPos hellper function to generate random position on map
+ * used for snake and food positioning
  */
 function randomPos() {
     return {
-        x: Math.floor(Math.random() * height) * tileSize,
-        y: Math.floor(Math.random() * width) * tileSize
+        x: Math.floor(Math.random() * height ) * tileSize, 
+        y: Math.floor(Math.random() * width ) * tileSize
     }
 }
-// user input for game controll
+/**
+ * Input function add event when ever key is pressed or buttons are clicked 
+ */
 function input() {
     lastInputDr = inputDr;
     document.body.addEventListener("keydown", event => {
         switch (event.key) {
             case "ArrowUp":
-                console.log("pressed up");
                 if (lastInputDr.y !== 0) break;
                 inputDr = { x: 0, y: -1 };
                 break;
@@ -220,7 +229,7 @@ function input() {
                 if (!isPaused) {
                     pauseGame();
                 } else {
-                    resumeGame();
+                    resetGame();
                 }
                 break;
 
@@ -248,25 +257,25 @@ function input() {
             case "startbtn":
                 console.log("start the Game!");
                 hideMenu(startUI);
-                currentlives = lives;
                 gameStart();
                 break;
             case "playAgainbtn":
                 hideMenu(gameOverUI);
                 isGameOver = false;
-                currentlives = lives;
+                clearInterval(gameLoop);
                 gameStart();
                 break;
         }
     })
 }
-
+/**
+ * Collision function to detect any collision with snake,wall and food.
+ */
 function collision() {
     //self collisions
     for (let i = 0; i < snakeBody.length - 1; i++) {
         if (snake.x === snakeBody[i][0] && snake.y === snakeBody[i][1]) {
             lives--;
-            currentlives = lives;
             hitEffect.volume = 0.5;
             hitEffect.play();
             resetGame();
@@ -288,7 +297,6 @@ function collision() {
         if (snake.x === wall.x && snake.y === wall.y) {
             // loose live and reset level.
             lives--;
-            currentlives = lives;
             hitEffect.volume = 0.5;
             hitEffect.play();
             resetGame();
@@ -296,52 +304,34 @@ function collision() {
         //detect if food is generated on snake or wall
         if (food.x === snake.x && food.y === snake.y || food.x === wall.x && food.y === wall.y) {
             food = randomPos();
-            eatEffect.volume = 0.5;
-            eatEffect.play();
         }
 
     }
 }
-function pauseGame() {
-    isPaused = true;
-}
-function resumeGame() {
-    isPaused = false;
-}
+
 function resetGame() {
     
-    if (currentlives <= 0) {
+    if (lives <= 0) {
         isGameOver = true;
         showMenu(gameOverUI);
-        lives = 3;
-        currentlives = lives;
-        isPaused = true;
-    } else if (currentlives >= 0) {
+    } else if (lives >= 0) {
         //set score back to 0
         score = 0;
         //reset snake 
         snakeBody.length = 1;
         inputDr.x = 0;
         inputDr.y = 0;
-        snake = randomPos();
+        snake = {x:120, y:220};
         food = randomPos();
         isPaused = false;
     }
 
 }
 
-
-/**
- * Audio background music
- */
 function muteAudio() {
     let backgroundAudio = document.getElementById("bgAudio");
     backgroundAudio.muteAudio = !backgroundAudio.muteAudio;
 }
-
-/**
- * Menu Functions 
- */
 
 function showMenu(menu) {
     menu.style.visibility = "visible";
